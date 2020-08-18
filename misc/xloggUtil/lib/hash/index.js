f56d9b7dd9efc6a1f228587958e0ae389ee3fd5a
Tom <tom.chesters96@gmail.com>
2020-08-18 15:01:41 +0100

    Feature : Util package Added hash & check password functionality

--- misc/xloggUtil/lib/hash/index.js
+++ misc/xloggUtil/lib/hash/index.js
@@ -1,7 +1,14 @@
+const bcrypt = require("bcrypt")
 
+const hashPassword = async (password) => {
+    const hashedPass = await bcrypt.hash(password, 8)
+    return hashedPass
+}
 
-const hashPassword = (password) => {
-    return password
+const checkPassword = async (password, hashedPass) => {
+    const match = await bcrypt.compare(password, hashedPass)
+    return match
 }
 
 exports.hashPassword = hashPassword
+exports.checkPassword = checkPassword
--- misc/xloggUtil/lib/index.js
+++ misc/xloggUtil/lib/index.js
@@ -1,11 +1,13 @@
 const apiWrapperCoreFns = require("./apiWrapper/core")
 const apiWrapperMongoFns = require("./apiWrapper/mongo")
+const hashFns = require("./hash")
 const lockArchetypeFns = require("./lock/archetype")
 const lockItemFns = require("./lock/item")
 
 module.exports = {
     ...apiWrapperCoreFns,
     ...apiWrapperMongoFns,
+    ...hashFns,
     ...lockArchetypeFns,
     ...lockItemFns
 }
